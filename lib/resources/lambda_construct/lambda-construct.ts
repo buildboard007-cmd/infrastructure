c9b8d05184e826a91643ee230fe8af335eb60e69
Mayur <mayur@mac.local.ftr>
2025-08-24 15:58:04 -0500

    Fix CloudFormation resource conflicts for Lambda security groups
    - Create shared Lambda security group once in LambdaConstruct instead of creating duplicate security groups in individual Lambda functions
    - Update FuncProps interface to support optional VPC configuration for database-accessing Lambdas
    - Refactor InfrastructureTokenCustomizer and InfrastructureUserSignup to use shared security group pattern
    - Remove duplicate VPC configuration logic and consolidate into centralized shared resources
    
    This resolves CloudFormation deployment failures caused by multiple security groups with same name being created simultaneously.

--- lib/resources/lambda_construct/lambda-construct.ts
+++ lib/resources/lambda_construct/lambda-construct.ts
@@ -5,24 +5,46 @@ import {LambdaConstructProps} from "../../types/lambda-construct-props";
 import {InfrastructureTokenCustomizer} from "../function_construct/infrastructure-token-customizer";
 import {InfrastructureUserSignup} from "../function_construct/infrastructure-user-signup";
 import {GoFunction} from "@aws-cdk/aws-lambda-go-alpha";
+import {SharedLambdaSecurityGroup} from "../security_construct/shared-lambda-security";
+import {getDatabaseVpcConfig} from "../../utils/database-vpc-config";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class LambdaConstruct extends Construct {
 
     private readonly infrastructureApiGatewayCors: InfrastructureApiGatewayCors;
     private readonly infrastructureTokenCustomizer: InfrastructureTokenCustomizer;
     private readonly infrastructureUserSignup: InfrastructureUserSignup;
+    private readonly sharedLambdaSG: SharedLambdaSecurityGroup;
 
     constructor(scope: Construct, id: string, props: LambdaConstructProps) {
         super(scope, id);
+
+        // Create shared VPC configuration and security group ONCE for all database-accessing Lambdas
+        const vpcConfig = getDatabaseVpcConfig(this, 'SharedVpcConfig');
+        this.sharedLambdaSG = new SharedLambdaSecurityGroup(this, "SharedLambdaSG", vpcConfig.vpc);
+
+        // Allow the shared Lambda security group to connect to RDS
+        vpcConfig.databaseSecurityGroup.addIngressRule(
+            this.sharedLambdaSG.securityGroup,
+            ec2.Port.tcp(5432),
+            "Allow all Lambdas with shared SG to connect to PostgreSQL"
+        );
 
         const funcProps: FuncProps = {
             options: props.options,
             stageEnvironment: props.stageEnvironment
         };
 
+        // Extended props for database-accessing Lambdas
+        const databaseFuncProps = {
+            ...funcProps,
+            vpcConfig,
+            sharedLambdaSG: this.sharedLambdaSG
+        };
+
         this.infrastructureApiGatewayCors = new InfrastructureApiGatewayCors(this, 'InfrastructureApiGatewayCors', funcProps);
-        this.infrastructureTokenCustomizer = new InfrastructureTokenCustomizer(this, 'InfrastructureTokenCustomizer', funcProps);
-        this.infrastructureUserSignup = new InfrastructureUserSignup(this, 'InfrastructureUserSignup', funcProps);
+        this.infrastructureTokenCustomizer = new InfrastructureTokenCustomizer(this, 'InfrastructureTokenCustomizer', databaseFuncProps);
+        this.infrastructureUserSignup = new InfrastructureUserSignup(this, 'InfrastructureUserSignup', databaseFuncProps);
     }
 
     get corsLambda(): GoFunction {
